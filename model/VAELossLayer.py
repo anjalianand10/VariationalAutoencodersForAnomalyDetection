beea7b36be2987d48e530f22bd6a3b1bf701e82e
Nikos Fotopoulos <nick.foto.ret@gmail.com>
2024-04-06 20:41:06 +0200

    window now has a vae

--- model/VAELossLayer.py
+++ model/VAELossLayer.py
@@ -9,6 +9,7 @@ class VAELossLayer(tf.keras.layers.Layer):
 
     def call(self, inputs):
         x, x_decoded_mean, z_mean, z_log_var, beta = inputs
+        x = tf.reshape(x, tf.shape(x_decoded_mean))
         reconstruction_loss = tf.reduce_sum(tf.square(x - x_decoded_mean))
         kl_loss = - 0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=-1)
         # return the average loss 
