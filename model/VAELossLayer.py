483d709dee5f54fc9f7865fad9eaa1a6cb52d47c
Anjali Anand <99anjali.anand@gmail.com>
2024-04-08 20:40:31 +0200

    Trained VAE on two new datasets

--- model/VAELossLayer.py
+++ model/VAELossLayer.py
@@ -1,18 +1,19 @@
 import tensorflow as tf
+from tensorflow import keras as keras
 
-class VAELossLayer(tf.keras.layers.Layer):
-    #
-    # Custom layer for VAE loss
-    #
+class VAELossLayer(keras.layers.Layer):
     def __init__(self, **kwargs):
         super(VAELossLayer, self).__init__(**kwargs)
 
     def call(self, inputs):
-        x, x_decoded_mean, z_mean, z_log_var, beta = inputs
+        x, x_decoded_mean, z_mean, z_log_var = inputs
+        # Reshape inputs to have the same shape
         x = tf.reshape(x, tf.shape(x_decoded_mean))
+        # compute the average MSE error, then scale it up, i.e. simply sum on all axes
         reconstruction_loss = tf.reduce_sum(tf.square(x - x_decoded_mean))
+        # compute the KL loss
         kl_loss = - 0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=-1)
-        # return the average loss 
-        total_loss = tf.reduce_mean(reconstruction_loss + beta * kl_loss)
+        # return the average loss over all 
+        total_loss = tf.reduce_mean(reconstruction_loss + kl_loss)    
         self.add_loss(total_loss)
         return x  # Dummy output
